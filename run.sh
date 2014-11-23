ca219cbfe4c4c6160ffc2bac6ad74a937f66ae31
abelsonlive <brianabelson@gmail.com>
2014-11-23 03:14:02 -0500

    updates

---
+++ run.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+
+# install xcode
+xcode-select --install
+
+# install homebrew + homebrew cask
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew install caskroom/cask/brew-cask
+
+# install pip
+sudo easy_install pip
+
+# install ansible 
+sudo pip install ansible 
+
+# run playbook
+ansible-playbook main.yml -i hosts --ask-sudo-pass -vvvv
+
